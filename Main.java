0cda0bb00c05da9fe2d1e378030698d55ee7edc1
Vitalik-bog <78750613+Vitalik-bog@users.noreply.github.com>
2021-06-15 10:27:59 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,13 +3,86 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        Animal zoo = new Animal();
+        zoo.makeNoise();
+        Veterinar doctor = new Veterinar();
+        doctor.treatAnimal();
+    }
+}
+
+class Animal {
+
+    public void makeNoise() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введи что нибудь уже");
-        String.word = in.nextLine();
-        StringBuffer str = new StringBuffer(word);
-        StringBuffer rts = new StringBuffer(str);
-        rts.reverse();
-        System.out.println(str.compareTo(rts));
+        System.out.println("Введите еду");
+        String food = in.nextLine();
+        System.out.println("Введите локацию");
+        String location = in.nextLine();
+        System.out.print(food + "\n" + location);
+
+    }
+
+    public void eat() {
+        System.out.println("ест");
+
+    }
+
+    public void sleep() {
+        System.out.println("спит");
+    }
+}
+
+class Dog extends Animal {
+    String Dog1 = "this.Dog1";
+
+    @Override
+    public void makeNoise() {
+        String Dog1 = new String("не спит");
+    }
+
+    @Override
+    public void eat() {
+        String Dog1 = new String("ест");
+    }
+}
+
+class Cat extends Animal {
+    String Cat1 = "this.Cat1";
+
+    @Override
+    public void makeNoise() {
+        String Cat1 = new String("не спит");
+
+    }
+
+    @Override
+    public void eat() {
+        String Cat1 = new String("ест");
+    }
+}
+
+class Horse extends Animal {
+    String Horse1 = "this.Horse1";
+
+
+    @Override
+    public void makeNoise() {
+        String Horse1 = new String("не спит");
+
+    }
+
+    @Override
+    public void eat() {
+        String Horse1 = new String("ест");
+    }
+}
+
+class Veterinar {
+    public void treatAnimal() {
+        Animal cat1 = new Cat();
+
+
     }
 }
